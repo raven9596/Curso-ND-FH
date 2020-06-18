b55a1ac7373d58314c8fbfe17a0f692eb0011711
Gilbert Acosta <acostagilberto95@gmail.com>
2020-06-18 12:44:54 -0400

    variable de entorno

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,35 +11,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuario', (req, res) => {
-  res.json('get Usuario');
-});
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', (req, res) => {
-  let body = req.body;
+mongoose.connect(
+  process.env.URLDB,
+  { useNewUrlParser: true, useCreateIndex: true },
+  (err, res) => {
+    if (err) throw err;
 
-  if (body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      mensaje: 'El nombre es necesario',
-    });
-  } else {
-    res.json({
-      persona: body,
-    });
-  }
-});
-
-app.put('/usuario/:id', (req, res) => {
-  let id = req.params.id;
-  res.json({
-    id,
-  });
-});
-
-app.delete('/usuario', (req, res) => {
-  res.json('delete Usuario');
-});
+    console.log('Base de datos ONLINE');
+  },
+);
 
 app.listen(process.env.PORT, () => {
   console.log('Escuchando por el puerto:', 3000);
